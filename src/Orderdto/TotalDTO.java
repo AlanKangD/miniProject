d8b4742e831a0d0937068c09cd00a6c7269ea5e5
soomin <nsm0428@gmail.com>
2021-09-29 02:48:00 +0900

    Orderdto에 TotalDTO추가했음

---
+++ src/Orderdto/TotalDTO.java
@@ -0,0 +1,99 @@
+package Orderdto;
+
+public class TotalDTO {
+	private int sales_code;
+	private int burger_1;
+	private int burger_2;
+	private int burger_3;
+	private int burger_4;
+	private int side_1;
+	private int side_2;
+	private int side_3;
+	private int side_4;
+	private int beverage_1;
+	private int beverage_2;
+	private int beverage_3;
+	private int beverage_4;
+	public int getSales_code() {
+		return sales_code;
+	}
+	public void setSales_code(int sales_code) {
+		this.sales_code = sales_code;
+	}
+	public int getBurger_1() {
+		return burger_1;
+	}
+	public void setBurger_1(int burger_1) {
+		this.burger_1 = burger_1;
+	}
+	public int getBurger_2() {
+		return burger_2;
+	}
+	public void setBurger_2(int burger_2) {
+		this.burger_2 = burger_2;
+	}
+	public int getBurger_3() {
+		return burger_3;
+	}
+	public void setBurger_3(int burger_3) {
+		this.burger_3 = burger_3;
+	}
+	public int getBurger_4() {
+		return burger_4;
+	}
+	public void setBurger_4(int burger_4) {
+		this.burger_4 = burger_4;
+	}
+	public int getSide_1() {
+		return side_1;
+	}
+	public void setSide_1(int side_1) {
+		this.side_1 = side_1;
+	}
+	public int getSide_2() {
+		return side_2;
+	}
+	public void setSide_2(int side_2) {
+		this.side_2 = side_2;
+	}
+	public int getSide_3() {
+		return side_3;
+	}
+	public void setSide_3(int side_3) {
+		this.side_3 = side_3;
+	}
+	public int getSide_4() {
+		return side_4;
+	}
+	public void setSide_4(int side_4) {
+		this.side_4 = side_4;
+	}
+	public int getBeverage_1() {
+		return beverage_1;
+	}
+	public void setBeverage_1(int beverage_1) {
+		this.beverage_1 = beverage_1;
+	}
+	public int getBeverage_2() {
+		return beverage_2;
+	}
+	public void setBeverage_2(int beverage_2) {
+		this.beverage_2 = beverage_2;
+	}
+	public int getBeverage_3() {
+		return beverage_3;
+	}
+	public void setBeverage_3(int beverage_3) {
+		this.beverage_3 = beverage_3;
+	}
+	public int getBeverage_4() {
+		return beverage_4;
+	}
+	public void setBeverage_4(int beverage_4) {
+		this.beverage_4 = beverage_4;
+	}
+	
+	
+	
+
+}
